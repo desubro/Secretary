4f1f08fe33c6c94999beea18e336f8083c2a743d
minedron <mainedeon@yandex.ru>
2015-12-01 17:14:46 +0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,8 +18,14 @@ using System.Windows.Shapes;
 
 //added
 using System.Windows.Threading;
+using System.Windows.Media.Animation;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+
 namespace WpfApplication1
 {
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -29,13 +35,12 @@ namespace WpfApplication1
         private DispatcherTimer dT = new DispatcherTimer();
         SpeechSynthesizer synthesizer = new SpeechSynthesizer();
         DispatcherTimer timer;
-        String path = "C:\\Users\\Vladislav\\Desktop\\BD.accdb";
+        String path = "C:\\Users\\user\\Desktop\\WpfApplication1\\WpfApplication1\\files\\BD.accdb";
         public MainWindow()
         {
             InitializeComponent();
             //Sets the time
             timer = new DispatcherTimer();
-            synthesizer.Rate = 3;
             timer.Interval = TimeSpan.FromSeconds(1.00);
             timer.Start();
             timer.Tick += new EventHandler(delegate (object s, EventArgs a)
@@ -50,13 +55,14 @@ namespace WpfApplication1
         private void btnEvents_Click(object sender, RoutedEventArgs e)
         {
             lblWhereWeAre.Content = "Мероприятия";
+            fullEventGrid.Visibility = Visibility.Hidden;
             eventGrid.Visibility = Visibility.Visible;
-            birhtDayGrid.Visibility = Visibility.Hidden;
-            newEventGrid.Visibility = Visibility.Hidden;
-            configGrid.Visibility = Visibility.Hidden;
             btnNewEvent.Visibility = Visibility.Visible;
-
+            newEventGrid.Visibility = Visibility.Hidden;
+            birhtDayGrid.Visibility = Visibility.Hidden;
+            homeGrid.Visibility = Visibility.Hidden;
             CDataBase.TableWrite(path, "Мероприятия", eventsGridData);
+            slideAnimationTo(eventGrid);
 
 
         }
@@ -64,98 +70,179 @@ namespace WpfApplication1
         private void btnBirthDay_Click(object sender, RoutedEventArgs e)
         {
             lblWhereWeAre.Content = "Дни Рождения";
-            eventGrid.Visibility = Visibility.Hidden;
-            birhtDayGrid.Visibility = Visibility.Visible;
+            fullEventGrid.Visibility = Visibility.Hidden;
             newEventGrid.Visibility = Visibility.Hidden;
-            configGrid.Visibility = Visibility.Hidden;
-            btnNewEvent.Visibility = Visibility.Hidden;
-
+            birhtDayGrid.Visibility = Visibility.Visible;
+            homeGrid.Visibility = Visibility.Hidden;
+            eventGrid.Visibility = Visibility.Hidden;
             CDataBase.TableWrite(path, "ДР", birthDayGridData);
+            slideAnimationTo(birhtDayGrid);
+
 
         }
 
         private void btnNewEvent_Click(object sender, RoutedEventArgs e)
         {
             lblWhereWeAre.Content = "Новое мероприятие";
+            fullEventGrid.Visibility = Visibility.Hidden;
             newEventGrid.Visibility = Visibility.Visible;
-            configGrid.Visibility = Visibility.Hidden;
-
-        }
-
-        private void btnConfigs_Click(object sender, RoutedEventArgs e)
-        {
-            lblWhereWeAre.Content = "Настройки";
-            eventGrid.Visibility = Visibility.Hidden;
+            dateOfEvent.DisplayDateStart = DateTime.Now;
             birhtDayGrid.Visibility = Visibility.Hidden;
-            newEventGrid.Visibility = Visibility.Hidden;
-            configGrid.Visibility = Visibility.Visible;
-            btnNewEvent.Visibility = Visibility.Hidden;
+            homeGrid.Visibility = Visibility.Hidden;
+            slideAnimationFrom(eventGrid);
+            slideAnimationTo(newEventGrid);
+            timeOfEvent.Text = "0:00";
+            tbEventName.Text = "Название";
+            tbEventDescription.Text = "Описание";
 
         }
 
         private void mainWindow_Initialized(object sender, EventArgs e)
         {
-            CDataBase.TableWrite(path, "Мероприятия", homeEventsGridData);
+            CDataBase.TableWriteLimit(path, "Мероприятия", homeEventsGridData);
+            CDataBase.TableWriteLimit(path, "ДР", homeBirthDayGridData);
             eventGrid.Visibility = Visibility.Hidden;
             birhtDayGrid.Visibility = Visibility.Hidden;
             newEventGrid.Visibility = Visibility.Hidden;
-            configGrid.Visibility = Visibility.Hidden;
+            fullEventGrid.Visibility = Visibility.Hidden;
             btnNewEvent.Visibility = Visibility.Hidden;
-        }
+            synthesizer.Rate = 2;
 
-        private void btnNewEvent_DragEnter(object sender, DragEventArgs e)
-        {
 
         }
 
         private void btnHomePage_Click(object sender, RoutedEventArgs e)
         {
-            lblWhereWeAre.Content = "Добро пожаловать";
-            eventGrid.Visibility = Visibility.Hidden;
-            birhtDayGrid.Visibility = Visibility.Hidden;
+
+            lblWhereWeAre.Content = "Электронный секретарь";
             newEventGrid.Visibility = Visibility.Hidden;
-            configGrid.Visibility = Visibility.Hidden;
-            btnNewEvent.Visibility = Visibility.Hidden;
-
-            CDataBase.TableWrite(path, "Мероприятия", homeEventsGridData);
-
+            fullEventGrid.Visibility = Visibility.Hidden;
+            birhtDayGrid.Visibility = Visibility.Hidden;
+            homeGrid.Visibility = Visibility.Visible;
+            eventGrid.Visibility = Visibility.Hidden;
+            CDataBase.TableWriteLimit(path, "Мероприятия", homeEventsGridData);
+            slideAnimationTo(homeGrid);
         }
 
         private void Window1_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             //Set the interval of the Tick event to 1 sec
             dT.Interval = new TimeSpan(0, 0, 1);
-
             //Start the DispatcherTimer
             dT.Start();
             secondArc.StartAngle = (DateTime.Now.Second * 6);
             minuteArc.StartAngle = (DateTime.Now.Minute * 6);
             hourArc.StartAngle = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5);
-            synthesizer.Speak("Добро пожаловать! Я ваш электронный секретарь!");
-
         }
 
+        //Добавление мероприятия
         private void btnAddEvent_Click(object sender, RoutedEventArgs e)
         {
             String date, name, description;
+            if (tbEventName.Text.ToString() == "") { MessageBox.Show("Вы не задали название мероприятия"); return; }
             name = tbEventName.Text.ToString();
+            if (tbEventDescription.Text.ToString() == "") { MessageBox.Show("Вы не задали описание мероприятия"); return; }
             description = tbEventDescription.Text.ToString();
-            date = dateOfEvent.Text.ToString() + " " + tbHours.Text.ToString() + ":" + tbMinuts.Text.ToString();
+            if (timeOfEvent.Text.ToString() == "") { MessageBox.Show("Вы не задали время мероприятия"); return; }
+            date = dateOfEvent.Text.ToString()+" "+timeOfEvent.Text.ToString();
+            if (DateTime.Parse(date) < DateTime.Now) { MessageBox.Show("Время заданного вами мероприятия прошло"); return; }
 
             CDataBase.insertEvent(path, date, name, description);
 
             lblWhereWeAre.Content = "Мероприятия";
-            eventGrid.Visibility = Visibility.Visible;
             birhtDayGrid.Visibility = Visibility.Hidden;
-            newEventGrid.Visibility = Visibility.Hidden;
-            btnNewEvent.Visibility = Visibility.Visible;
-
+            homeGrid.Visibility = Visibility.Hidden;
+            fullEventGrid.Visibility = Visibility.Hidden;
+            eventGrid.Visibility = Visibility.Visible;
             CDataBase.TableWrite(path, "Мероприятия", eventsGridData);
+            slideAnimationTo(eventGrid);
+            slideAnimationFrom(newEventGrid);
+            MessageBox.Show("Мероприятие успешно добавлено!");
         }
 
+        //Переход на сайт миэт при клике по картинке
         private void image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Process.Start("http://miet.ru/");
+        }
+
+        //Удаление мероприятия
+        private void btnDeleteEvent_Click(object sender, EventArgs e)
+        {
+            int code, parse;
+            parse = eventsGridData.SelectedIndex;
+            DataRowView dataRow = eventsGridData.SelectedValue as DataRowView;
+            code = int.Parse(dataRow[0].ToString());
+            var mbResult = MessageBox.Show("Вы действительно хотите удалить мероприятие?", "Удаление мероприятия",
+         MessageBoxButton.YesNo);
+            if (mbResult == MessageBoxResult.Yes)
+            {
+                CDataBase.EventRowDelete(path, code);
+                CDataBase.TableWrite(path, "Мероприятия", eventsGridData);
+            }
+
+        }
+
+        private void eventsGridData_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            int parse;
+            slideAnimationFrom(eventGrid);
+            slideAnimationTo(fullEventGrid);
+            parse = eventsGridData.SelectedIndex;
+            DataRowView dataRow = eventsGridData.SelectedValue as DataRowView;
+            showDateOfEvent.DisplayDateStart = DateTime.Parse(dataRow[1].ToString());
+            showDateOfEvent.DisplayDateEnd = DateTime.Parse(dataRow[1].ToString());
+            showDateOfEvent.SelectedDate = DateTime.Parse(dataRow[1].ToString());
+            lblWhereWeAre.Content = dataRow[2].ToString();
+            lblShowEventName.Content = dataRow[2].ToString();
+            lblShowEventTime.Content = (DateTime.Parse(dataRow[1].ToString())).ToString("HH:mm:ss");
+            lblShowEventDescription.Text = dataRow[3].ToString();
+            fullEventGrid.Visibility = Visibility.Visible;
+        }
+
+        //Анимации панелей
+        private void slideAnimationTo(Grid gridName)
+        {
+            Storyboard sb = new Storyboard();
+
+            DoubleAnimation slide = new DoubleAnimation();
+            slide.To = 0;
+            slide.From = mainWindow.Width * 1.5;
+            slide.Duration = new Duration(TimeSpan.FromMilliseconds(300.0));
+
+            // Set the target of the animation
+            Storyboard.SetTarget(slide, gridName);
+            Storyboard.SetTargetProperty(slide,
+    new PropertyPath("RenderTransform.(TranslateTransform.X)"));
+            // Kick the animation off
+            sb.Children.Add(slide);
+            sb.Begin();
+        }
+
+        private void btnBackToEvent_Click(object sender, RoutedEventArgs e)
+        {
+            lblWhereWeAre.Content = "Мероприятия";
+            CDataBase.TableWrite(path, "Мероприятия", eventsGridData);
+            slideAnimationTo(eventGrid);
+            slideAnimationFrom(fullEventGrid);
+        }
+
+        private void slideAnimationFrom(Grid gridName)
+        {
+            Storyboard sb = new Storyboard();
+
+            DoubleAnimation slide = new DoubleAnimation();
+            slide.To = -mainWindow.Width * 3;
+            slide.From = 0;
+            slide.Duration = new Duration(TimeSpan.FromMilliseconds((3 / 2) * 300.0));
+
+            // Set the target of the animation
+            Storyboard.SetTarget(slide, gridName);
+            Storyboard.SetTargetProperty(slide,
+    new PropertyPath("RenderTransform.(TranslateTransform.X)"));
+            // Kick the animation off
+            sb.Children.Add(slide);
+            sb.Begin();
         }
     }
 }
